8cc437eadf259df76d8308f94d45b981bb670fc7
artistatbl <jdaly2991@gmail.com>
2026-03-25 14:38:26 +0000

    feat: move `Listening` component after `FeaturesSection` on the homepage

--- apps/web/src/app/(site)/page.tsx
+++ apps/web/src/app/(site)/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
         <SectionStack>
           <Hero />
           <ConversionHero />
-          <Listening />
           <FeaturesSection />
+          <Listening />
           <Integrations />
           <Setup />
           <Create />
