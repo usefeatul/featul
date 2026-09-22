049ccbc60575bb8451eb4d30444a79993006174b
artistatbl <jdaly2991@gmail.com>
2026-09-22 17:47:00 +0100

    style: unify clock and workspace iconography
    - Replace legacy clock and workspace timer paths with consistent 24px clock icons
    - Update workspace creation icon geometry

--- packages/ui/src/icons/clock.tsx
+++ packages/ui/src/icons/clock.tsx
@@ -16,32 +16,20 @@ export const ClockIcon: React.FC<ClockIconProps> = ({
       xmlns="http://www.w3.org/2000/svg"
       width={size}
       height={size}
-      viewBox="0 0 18 18"
+      viewBox="0 0 24 24"
       opacity={opacity}
       className={className}
       aria-hidden="true"
     >
       <g
+        transform="translate(0.75 0.75) scale(0.9375)"
         fill="none"
         stroke="currentColor"
-        strokeWidth="1.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <path d="M6.75 1.25H11.25" />
-        <path d="M9 1.25V3.75" />
-        <path
-          d="M9 16.25C12.4518 16.25 15.25 13.4518 15.25 10C15.25 6.54822 12.4518 3.75 9 3.75C5.54822 3.75 2.75 6.54822 2.75 10C2.75 13.4518 5.54822 16.25 9 16.25Z"
-          className="origin-center transition-transform duration-200 ease-out group-hover:scale-105 group-active:scale-95"
-        />
-        <path
-          d="M6.70203 7.702L9.00003 10"
-          className="origin-[9px_10px] transition-transform duration-300 ease-out group-hover:-rotate-12"
-        />
-        <path
-          d="M14.25 2.75L16.25 4.75"
-          className="transition-transform duration-200 ease-out group-hover:translate-x-px group-hover:-translate-y-px"
-        />
+        <path d="M12 6V12L16 14M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" />
       </g>
     </svg>
   );
